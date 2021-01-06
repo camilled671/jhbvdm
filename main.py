6fae8173e4d542d9edd12e461f32066b083d750a
camilled671 <72838912+camilled671@users.noreply.github.com>
2021-01-05 21:45:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 while True:
     print("light level:" + str(input.light_level()))
-    if input.light_level() >=8:
+    if input.light_level() >=10:
         light.set_all(light.rgb(128,0,128))
     else:
-        if input.light_level() =<7:
+        if input.light_level() =<9:
             light.set_all(light.rgb(0, 255, 255))
